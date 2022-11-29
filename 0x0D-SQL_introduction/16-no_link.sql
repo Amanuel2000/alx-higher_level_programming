39bd72980263cc6639353f02327cad10a4cd194b
Amanuel2000 <amanuelf44@gmail.com>
2022-11-30 00:31:43 +0300

    solution

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,4 @@
+-- a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+-- list rows without a name value
+
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
