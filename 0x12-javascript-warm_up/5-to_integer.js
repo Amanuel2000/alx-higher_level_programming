451a817b56b26ba9ce5397637b7e149c91deae29
Amanuel2000 <amanuelf44@gmail.com>
2022-12-19 22:59:05 +0300

    Java Script warm up: Arguments

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-console.log(parseInt(process.argv[2]) ? `My number: ${parseInt(process.argv[2])}` : 'Not a number')
+
+if (process.argv[2] === undefined) {
+    console.log('Not a number');
+  } else {
+    console.log('My number: ' + parseInt(process.argv[2]));
+  }
+  
