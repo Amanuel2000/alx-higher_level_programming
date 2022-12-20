04823f6b633cbc50e3d80d9c363cdee8a0297892
Amanuel2000 <amanuelf44@gmail.com>
2022-12-20 19:49:08 +0300

    JavaScript - Objects, Scopes and Closures: Square class created and inherit from Rectangle

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -2,12 +2,12 @@
 /**
  * Square class that inherits from rectangle class
  */
- const Rectangle = require('./4-rectangle.js');
+const Rectangle = require('./4-rectangle.js');
 
- class Square extends Rectangle {
-   constructor (size) {
-     super(size, size);
-   }
- }
- 
- module.exports = Square;
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Square;
