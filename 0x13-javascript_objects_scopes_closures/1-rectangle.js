0b1e3e68200acf2bca27d87fd21167d18ea3d446
Amanuel2000 <amanuelf44@gmail.com>
2022-12-20 19:06:59 +0300

    JavaScript - Objects, Scopes and Closures: Rectangle class created

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+class Rectangle {
+    h;
+    w;
+    
+    constructor(h, w){
+        this.width = w;
+        this.height = h;
+
+    }
+
+}
---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+    h;
+    w;
+    
+    constructor(h, w){
+        this.width = w;
+        this.height = h;
+
+    }
+    if (w === 0 || w <= 0 || h === 0 || h <= 0)
+        empty_obj = Rectangle();
+
+}
