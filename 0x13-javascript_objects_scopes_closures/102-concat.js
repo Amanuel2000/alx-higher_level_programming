460973a7d0dfab10ecab7b24817984e996141989
Amanuel2000 <amanuelf44@gmail.com>
2022-12-20 20:28:20 +0300

    JavaScript - Objects, Scopes and Closures: Advanced tasks

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-/**
- * a script that concats 2 files.
- * The first argument is the file path of the first source file
- * The second argument is the file path of the second source file
- * The third argument is the file path of the destination
- */
 
 const fs = require('fs');
 const a = fs.readFileSync(process.argv[2], 'utf8');
