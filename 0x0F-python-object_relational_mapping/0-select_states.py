208ff7d7e92322cee3469c7d75cbdc1c9f41765a
Amanuel2000 <amanuelf44@gmail.com>
2022-12-21 15:37:14 +0300

    Python - Object-relational mapping: Select states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Lists all states from the database hbtn_0e_0_usa.
+Usage: ./0-select_states.py <mysql username> \
+                            <mysql password> \
+                             <database name>
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    d_base = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], host="localhost", port=3306,)
+    d_cursor = d_base.cursor()
+    d_cursor.execute("SELECT * FROM `states`")
+    [print(state) for state in d_cursor.fetchall()]
